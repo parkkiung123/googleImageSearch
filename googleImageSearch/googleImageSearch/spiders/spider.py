5f83f126bb1ffb75e2d324a857a4658431c48def
parkkiung123 <parkkiung123@gmail.com>
2018-01-26 09:52:16 +0900

    Update spider.py

--- googleImageSearch/googleImageSearch/spiders/spider.py
+++ googleImageSearch/googleImageSearch/spiders/spider.py
@@ -40,7 +40,6 @@ class SpiderSpider(scrapy.Spider):
     headers['User-Agent'] \
         = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
     key_words = [
-		'石原さとみ',
         'Cristiano Ronaldo',
         'Lionel Messi'
     ]
